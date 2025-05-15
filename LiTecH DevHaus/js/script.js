18f2475429d3cc91c5ed084c477b0a12c5cfa41f
uLikaYE <136790997+Ulikaye@users.noreply.github.com>
2025-05-15 20:39:15 +0300

    Add files via upload

---
+++ LiTecH DevHaus/js/script.js
@@ -0,0 +1,18 @@
+
+// Popup functions to show/hide projects details
+function showPopupL() {
+  document.getElementById("L").style.display = "flex";
+}
+
+function hidePopupL() {
+  document.getElementById("L").style.display = "none";
+}
+
+// Popup functions to show/hide contact details
+function showPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function hidePopup() {
+  document.getElementById("popup").style.display = "none";
+}
